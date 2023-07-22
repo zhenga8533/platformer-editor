7fc89d7cd46b880263e069b8e4546b71037e533b
Allen Zheng <volcaronitee8533@gmail.com>
2023-07-22 11:12:17 -0400

    Added tilemap

--- editor.py
+++ editor.py
@@ -1,11 +1,13 @@
 import pygame
+from pygame import key
 from pygame.locals import *
 from util.constants import *
 
 
 class Editor:
     def __init__(self):
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((WIDTH + SIDEBAR_WIDTH, HEIGHT))
         pygame.display.set_caption('Stage Editor')
 
         self.level = 0
@@ -17,8 +19,31 @@ class Editor:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            elif event.type == pygame.KEYDOWN and event.key == K_UP:
+                self.level = (self.level + 1) % 43
+                self.background = pygame.image.load(BACKGROUND[self.level]).convert_alpha()
+                self.background = pygame.transform.scale(self.background, (WIDTH, HEIGHT))
+
+        # Player control
+        keys = key.get_pressed()
+        if keys[K_q]:
+            pygame.quit()
+            quit()
+
         self.draw()
+        self.clock.tick(FPS)
 
     def draw(self):
-        self.screen.blit(self.background, (0, 0))
+        self.draw_background()
         pygame.display.flip()
+
+    # Draws background and grid
+    def draw_background(self):
+        # background
+        self.screen.blit(self.background, (0, 0))
+
+        # 16px16p grid
+        for col in range(COLUMNS + 1):
+            pygame.draw.line(self.screen, GRAY, (col * TILE_SIZE, 0), (col * TILE_SIZE, HEIGHT))
+        for row in range(ROWS + 1):
+            pygame.draw.line(self.screen, GRAY, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
--- util/constants.py
+++ util/constants.py
@@ -1,3 +1,16 @@
-WIDTH = 900
-HEIGHT = 600
+# pygame constants
+WIDTH = 960
+HEIGHT = 640
+SIDEBAR_WIDTH = 320
+FPS = 60
+
+# game constants
 BACKGROUND = [f'assets/background/{i}.png' for i in range(1, 44)]
+TILE_SIZE = 16
+ROWS = HEIGHT // TILE_SIZE
+COLUMNS = WIDTH // TILE_SIZE
+
+# colors rgb
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (128, 128, 128)
